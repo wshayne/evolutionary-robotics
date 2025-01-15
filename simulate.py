b08db4749dd54bf6851f72fb07beb536d0442959
Will <wshayne@uvm.edu>
2025-01-15 17:40:52 -0500

    completed simulation

---
+++ simulate.py
@@ -0,0 +1,13 @@
+import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)
+
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+
+p.disconnect()
