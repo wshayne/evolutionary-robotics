05e76e517613dd842a60256ea1acc169d5ef014b
Will <wshayne@uvm.edu>
2025-01-23 19:39:37 -0500

    created basic world

--- generate.py
+++ generate.py
@@ -3,9 +3,6 @@ import pyrosim.pyrosim as pyrosim
 (length, width, height) = (1, 1, 1)
 (x, y, z) = (0, 0, .5)
 
-pyrosim.Start_SDF("boxes.sdf")
-for a in range(5):
-    for b in range(5):
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x+a,y+b,z+i], size=[length*.9**i,width*.9**i,height*.9**i])
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -8,7 +8,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
 for i in range(1000):
     p.stepSimulation()
