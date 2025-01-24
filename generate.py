0198886bd531f905ab406f8cc3dd868bb8ec55e2
Will <wshayne@uvm.edu>
2025-01-23 20:05:55 -0500

    relative coordinate arch

--- generate.py
+++ generate.py
@@ -7,9 +7,19 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5], size=[1,1,1])
-    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position=[0.5, 0, 1.0])
+    pyrosim.Send_Cube(name="Link_0", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Link_1", pos=[0,0,.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link_0_Link_1", parent="Link_0", child="Link_1", type="revolute", position=[0, 0, 1.0])
+    pyrosim.Send_Joint(name="Link_1_Link_2", parent="Link_1", child="Link_2", type="revolute", position=[0,0,1])
+    pyrosim.Send_Cube(name="Link_2", pos=[0,0,.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link_2_Link_3", parent="Link_2", child="Link_3", type="revolute", position=[0,.5,.5])
+    pyrosim.Send_Cube(name="Link_3", pos=[0,.5,0], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link_3_Link_4", parent="Link_3", child="Link_4", type="revolute", position=[0,1,0])
+    pyrosim.Send_Cube(name="Link_4", pos=[0,.5,0], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link_4_Link_5", parent="Link_4", child="Link_5", type="revolute", position=[0,.5,-.5])
+    pyrosim.Send_Cube(name="Link_5", pos=[0,0,-.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Link_5_Link_6", parent="Link_5", child="Link_6", type="revolute", position=[0,0,-1])
+    pyrosim.Send_Cube(name="Link_6", pos=[0,0,-.5], size=[1,1,1])
     pyrosim.End()
 
 if __name__ == "__main__":
