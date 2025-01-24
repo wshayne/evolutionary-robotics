b31bcb30d022faba5cc48c69ddb81c344900fabd
Will <wshayne@uvm.edu>
2025-01-23 19:54:02 -0500

    incorrect absolute positions

--- generate.py
+++ generate.py
@@ -1,8 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-(length, width, height) = (1, 1, 1)
-(x, y, z) = (0, 0, .5)
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-3,3,.5], size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.Start_SDF("world.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position=[0.5, 0, 1.0])
+    pyrosim.End()
+
+if __name__ == "__main__":
+    Create_World()
+    Create_Robot()
--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 for i in range(1000):
